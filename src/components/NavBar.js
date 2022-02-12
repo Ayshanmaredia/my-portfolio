30d13dcf4a52a6605483941562d4a1c10a5de349
Ayshanmaredia <58998373+Ayshanmaredia@users.noreply.github.com>
2022-02-11 21:15:21 -0500

    Add own logo and favicon & Add new file _redirects

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,12 @@ import { SiGithub, SiLinkedin } from 'react-icons/si';
 const NavBar = ({ showLinks = true }) => {
 
     return (
-        <nav className="sticky top-0 w-full bg-slate-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <nav className="sticky top-0 w-full bg-slate-50 border-gray-200 px-4 py-2.5 md:px-32 rounded dark:bg-gray-800">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="/" className="flex">
-                    <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Ayshan Maredia</span>
+                    <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
+                        <img className="w-10" src={require("../assets/logo.png")} alt="programmerImage" />
+                    </span>
                 </a>
                 <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span className="sr-only">Show menu</span>
