938f196eab9ae655a3198b90c810c2b7a7238467
Ayshanmaredia <58998373+Ayshanmaredia@users.noreply.github.com>
2022-02-12 21:23:38 -0500

    Delete Card.js and Minor change in Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -21,7 +21,7 @@ import { motion, useViewportScroll, useTransform } from 'framer-motion'
 const Skills = () => {
 
   const { scrollYProgress } = useViewportScroll()
-  const scale = useTransform(scrollYProgress, [0, 2], [0.2, 2]);
+  const scale = useTransform(scrollYProgress, [0, 1.5], [0.2, 1.5]);
 
   const skills = [{
     "name": "React",
