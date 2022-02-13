6110d4e16e4ef615602fd4e8770ad68915a9505d
Ayshanmaredia <58998373+Ayshanmaredia@users.noreply.github.com>
2022-02-12 21:17:22 -0500

    Add animation using framer-motion in Skill.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,8 +16,12 @@ import {
   SiMongodb,
   SiAdobephotoshop
 } from 'react-icons/si';
+import { motion, useViewportScroll, useTransform } from 'framer-motion'
 
 const Skills = () => {
+
+  const { scrollYProgress } = useViewportScroll()
+  const scale = useTransform(scrollYProgress, [0, 2], [0.2, 2]);
 
   const skills = [{
     "name": "React",
@@ -79,8 +83,13 @@ const Skills = () => {
 
   return (
     <section id="skills">
-      <div className="flex px-6 py-20 mx-auto my-2 text-center items-center justify-center md:px-80">
-        <div>
+      <motion.div
+        style={{ scale }}
+        className="flex px-6 py-20 mx-auto my-2 text-center items-center justify-center md:px-80">
+        <motion.div
+          style={{
+            scaleY: scrollYProgress
+          }}>
           <h4 className="text-3xl font-bold my-5">Skills and Tools</h4>
           {skills.map(({ name, Icon }, index) => (
             <Pill
@@ -89,8 +98,8 @@ const Skills = () => {
               Icon={Icon}
             />
           ))}
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   )
 };
